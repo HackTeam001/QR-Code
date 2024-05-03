d9e2751afa1351641f332037c22cf375afc95948
issa99-lab <issanana99@gmail.com>
2024-05-03 11:27:09 +0300

    Completed

--- backend/src/QrCode.sol
+++ backend/src/QrCode.sol
@@ -16,14 +16,19 @@ contract QrCode {
     uint256 public currentTime;
 
     mapping(address manufacturer => bool) public verifiedManufacturer;
+    mapping(address manufacturer => address retailer)
+        public manufacturerToRetailers;
     mapping(address manufacturer => mapping(address retailer => bool))
-        public manufacturerToRetailer;
+        public verifiedRetailer;
     mapping(address retailer => bool) public isRetailer;
 
     mapping(uint256 batchNumber => address manufacturer)
         public batchNumberToManufacturer;
     mapping(uint256 productId => uint256 batchNumber)
         public productIDsToBatchNumbers;
+    mapping(uint256 productId => address manufacturer)
+        public productIDsToManufacturer;
+
     mapping(uint256 productId => bool) public isStored;
     mapping(uint256 productId => bool) public itemSold;
 
@@ -31,10 +36,11 @@ contract QrCode {
         i_systemOwner = _owner;
     }
 
-    function addManufacturer(address manufacturer) external {
+    function addManufacturer(address _manufacturer) external {
+        require(msg.sender != address(0), "Invalid address");
         require(msg.sender == i_systemOwner, "Action only taken by owner");
-        emit ManufacturerAdded(manufacturer);
-        verifiedManufacturer[manufacturer] = true;
+        emit ManufacturerAdded(_manufacturer);
+        verifiedManufacturer[_manufacturer] = true;
     }
 
     function addRetailer(address _retailer) external {
@@ -42,61 +48,108 @@ contract QrCode {
             verifiedManufacturer[msg.sender] == true,
             "Action only taken by a verified manufacturer"
         );
+        require(_retailer != address(0), "Invalid address");
         emit RetailerAdded(msg.sender, _retailer);
-        manufacturerToRetailer[msg.sender][_retailer] = true;
+        manufacturerToRetailers[msg.sender] = _retailer;
+        verifiedRetailer[msg.sender][_retailer] = true;
         isRetailer[_retailer] = true;
     }
 
-    function storeItem(uint256 _batchNumber, uint256 _productId) external {
+    function storeItem(
+        uint256 _batchNumber,
+        uint256 _productId
+    ) external returns (uint256) {
         require(
             verifiedManufacturer[msg.sender] == true,
             "Action only taken by a verified manufacturer"
         );
         emit ItemStored(_batchNumber, _productId);
-        currentTime = block.timestamp;
         batchNumberToManufacturer[_batchNumber] = msg.sender;
         productIDsToBatchNumbers[_productId] = _batchNumber;
+        productIDsToManufacturer[_productId] = msg.sender;
         isStored[_productId] = true;
         itemSold[_productId] = false;
+        currentTime = block.timestamp;
+        return currentTime;
     }
 
-    function deleteStoredItem(uint256 _productId) external {
+    function deleteStoredItem(uint256 _productId) external returns (uint256) {
         require(
             verifiedManufacturer[msg.sender] == true,
             "Action only taken by a verified manufacturer"
         );
         emit DeletedItem(_productId);
-        currentTime = block.timestamp;
         delete productIDsToBatchNumbers[_productId];
         isStored[_productId] = false;
+        currentTime = block.timestamp;
+        return currentTime;
     }
 
-    //return with frontend Item Details
-    //not sure for the returns
-    /* function scanItem(
-        uint256 _productId
-    ) public view returns (bool, uint256, address, bool) {
+    //Return with frontend Item Details (Description, manufacturer)
+    function scanItem(uint256 _productId) public view returns (bool) {
         if (isStored[_productId] == true) {
-            uint256 batchNumber = productIDsToBatchNumbers[_productId];
-            address manufacturer = batchNumberToManufacturer[batchNumber];
-            bool item_sold = itemSold[_productId];
-            return (true, batchNumber, manufacturer, item_sold);
+            return true;
         }
-        else return false;
+        return false;
     }
 
-    function itemIsSold(
-        uint256 _productId
-    ) external view returns (uint256, bool) {
+    function itemIsSold(uint256 _productId) external returns (uint256) {
         require(
             isRetailer[msg.sender] == true,
             "Action only taken by a verified retailer"
         );
-    
-            emit ItemSold(_productId);
-            currentTime = block.timestamp;
-            itemSold[_productId] = true;
-            return currentTime;
 
-    } */
+        emit ItemSold(_productId);
+        currentTime = block.timestamp;
+        itemSold[_productId] = true;
+        return currentTime;
+    }
+
+    //GETTER FUNCTIONS
+    function checkIfManufacturer(
+        address _manufacturer
+    ) external view returns (bool) {
+        bool isManufacturer = verifiedManufacturer[_manufacturer];
+        return isManufacturer;
+    }
+
+    function checkIfRetailer(address _retailer) external view returns (bool) {
+        bool retailer = isRetailer[_retailer];
+        return retailer;
+    }
+
+    function getBatchNumberBasedOnProductId(
+        uint256 _productID
+    ) external view returns (uint256) {
+        uint256 batch = productIDsToBatchNumbers[_productID];
+        return batch;
+    }
+
+    function getManufacturerAddressBasedOnProductId(
+        uint256 _productID
+    ) external view returns (address) {
+        address manufacturer = productIDsToManufacturer[_productID];
+        return manufacturer;
+    }
+
+    function getManufacturerAddressBasedOnBatchNumber(
+        uint256 _batchNumber
+    ) external view returns (address) {
+        address manufacturer = batchNumberToManufacturer[_batchNumber];
+        return manufacturer;
+    }
+
+    function checkIfItemIsStored(
+        uint256 _productID
+    ) external view returns (bool) {
+        bool stored = isStored[_productID];
+        return stored;
+    }
+
+    function checkIfItemIsSold(
+        uint256 _productID
+    ) external view returns (bool) {
+        bool sold = itemSold[_productID];
+        return sold;
+    }
 }
